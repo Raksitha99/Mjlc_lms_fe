07c62bb8b6a4559f8e3bfd97b850998da28a2999
Raksitha99 <rakshithakulasara19@gmail.com>
2026-05-31 10:53:38 +0530

    Dev : change css file to scss.

--- src/app/layouts/auth-layout/auth-layout.ts
+++ src/app/layouts/auth-layout/auth-layout.ts
@@ -6,6 +6,6 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './auth-layout.html',
-  styleUrl: './auth-layout.css'
+  styleUrl: './auth-layout.scss'
 })
 export class AuthLayoutComponent {}
